6041e89d320746d213435acfeb6a6bdee1baf3ca
Anjitkumaryadav <anjitaniket@gmail.com>
2024-10-11 15:17:41 +0530

    setup authentication

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import conf from './conf/conf'
+
+function App() {
+  // console.log(appwriteUrl)
+  return (
+    <div>
+      <h1>blog with appwrite</h1>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem iusto optio rerum magnam et expedita atque veritatis repudiandae aliquam consequuntur dolores voluptates, totam quos quisquam eos sit accusantium! Doloribus, at ab? Ut modi dolorum expedita pariatur debitis obcaecati consequuntur tenetur voluptate iste. Excepturi pariatur repellendus vero fugiat quam? Necessitatibus tempore laudantium dolorum sint voluptatum saepe atque molestias deleniti quo cum ullam molestiae rem labore, aperiam repellendus consectetur in veniam, ab officiis aliquid. Modi magni recusandae nulla ut tempora alias soluta autem ea illo sit. Fuga ab voluptatem repellat. Numquam nam reiciendis repellendus, ipsa veniam blanditiis ducimus iure sunt atque deserunt.</div>
+    </div>
+  )
+}
+
+export default App
