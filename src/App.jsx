9894c9893fe2e2aef6422e84e105f2466bdb4b4c
Anjitkumaryadav <anjitaniket@gmail.com>
2024-10-24 02:16:00 +0530

    setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import conf from './conf/conf'
+import { useDispatch } from 'react-redux';
+import AuthService from './appwrite/auth';
+import{login, logout} from './store/authSlice'
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
+import authService from './appwrite/auth'
 
 function App() {
-  // console.log(appwriteUrl)
-  return (
-    <div>
-      <h1>blog with appwrite</h1>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem iusto optio rerum magnam et expedita atque veritatis repudiandae aliquam consequuntur dolores voluptates, totam quos quisquam eos sit accusantium! Doloribus, at ab? Ut modi dolorum expedita pariatur debitis obcaecati consequuntur tenetur voluptate iste. Excepturi pariatur repellendus vero fugiat quam? Necessitatibus tempore laudantium dolorum sint voluptatum saepe atque molestias deleniti quo cum ullam molestiae rem labore, aperiam repellendus consectetur in veniam, ab officiis aliquid. Modi magni recusandae nulla ut tempora alias soluta autem ea illo sit. Fuga ab voluptatem repellat. Numquam nam reiciendis repellendus, ipsa veniam blanditiis ducimus iure sunt atque deserunt.</div>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=> {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  }, [])
+  
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
     </div>
-  )
+  ) : (null)
 }
 
 export default App
